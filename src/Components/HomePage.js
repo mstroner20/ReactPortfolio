7095aee1bca5fb65ecc4c33a1dfec216192c4e6d
Michael Stroner <56314014+mstroner20@users.noreply.github.com>
2021-01-19 20:21:37 -0600

    General structure is complete

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -1,17 +1,35 @@
 import React from 'react'
- import '../css/about.css'
-
+import '../css/about.css'
+import Typical from 'react-typical'
 
 function HomePage(){
     return(
         <div >
-            
            <div className = "homepage-text">
-               <h3>Hi; </h3><h3>My name is Mike Stroner;</h3>
-               <p>//Software Engineer</p>
+               
+               <h3>Hi;</h3>
+               
+               
+               <h3>My name is Mike Stroner;</h3>
+               
+               
+               
+               <p className = "homePageParagraph">
+                   {''}
+                   <Typical
+                     loop = {Infinity}
+                     wrapper = 'p'
+                     steps = {[
+                        '//Software Engineer', 1000,
+                        
+                     ]}
+                    />
+                </p>
                <div className = "contact-button"><button>Contact Me!</button></div>
 
            </div>
+
+       
         </div>
     )
 }
--- src/Components/sidebar.jsx
+++ src/Components/sidebar.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
 
-import '../css/homePage.css';
+import '../css/sideBar.css';
+
+import HomeLogo from '../imgs/house.png'
+import About from '../imgs/about.svg'
+import ProjectLogo from '../imgs/projects.png'
+import ResumeLogo from '../imgs/resume.png'
+
 
 
 
@@ -11,9 +17,12 @@ function HomePage(){
 return(
     <div className = "left-nav">
         <div className = "side-nav-links">
+            <a href=""><img src = {HomeLogo}></img></a>
+            <a href = ""><img src = {About}></img></a>
+            <a href=""><img src = {ProjectLogo}></img></a>
+            <a href = ""><img src = {ResumeLogo}></img></a>
             
         </div>
-        
     </div>
 )
 
